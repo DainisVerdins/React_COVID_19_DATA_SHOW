581f7079fd3dcf50d090e279acc30cc19c9122b6
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-03-23 16:07:58 +0200

    feat: make datapickers in russian

--- src/DataPickerNavbar.js
+++ src/DataPickerNavbar.js
@@ -1,50 +1,57 @@
-import DatePicker from 'react-datepicker';
+import DatePicker, { registerLocale} from 'react-datepicker';
 import Constants from './constants/constants'
 
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+import ru from 'date-fns/locale/ru'; // set local to ru language
+registerLocale('ru', ru);
+
 // README: TODO: 
 /* 
-    1. Make datapickers format local not only english
-    2. Fix column problem for datapickers and text between them like it is in the task
+    1. Move locale to constants to change if needed
 */
-const DataPickerNavbar = ({minDate, maxDate, selectedStartDate, selectedEndDate, handleStartDateChange, handleEndDateChange}) => {
+const DataPickerNavbar = ({ minDate, maxDate, selectedStartDate, selectedEndDate, handleStartDateChange, handleEndDateChange }) => {
     return (
         <div className="data-picker-navbar-container">
             <Row className="align-items-start">
                 <Col xl={6}>
-                    <div>
-                    <p> Период от </p>
+                    <div className='data-picker-navbar-row'>
+                    <div className="data-picker-navbar-element">
+                        <h2> Период от </h2>
                     </div>
-                    <div>
-                    <DatePicker
-                        dateFormat={Constants.DATE_FORMAT}
-                        showIcon
-                        selected={selectedStartDate || minDate}
-                        minDate={minDate}
-                        maxDate={maxDate}
-                        onChange={(date) => handleStartDateChange(date)}
-                        calendarStartDay={1} //sets datepicker week day one
-                    />
+                    <div className="data-picker-navbar-element">
+                        <DatePicker
+                            dateFormat={Constants.DATE_FORMAT}
+                            showIcon
+                            selected={selectedStartDate || minDate}
+                            minDate={minDate}
+                            maxDate={maxDate}
+                            onChange={(date) => handleStartDateChange(date)}
+                            calendarStartDay={1} //sets datepicker week day one
+                            locale='ru'
+                        />
                     </div>
-                    <div>
-                    <p> до </p>
-
+                    <div className="data-picker-navbar-element">
+                        <h2> до </h2>
                     </div>
-                    <DatePicker
-                        dateFormat={Constants.DATE_FORMAT}
-                        showIcon
-                        selected={selectedEndDate || maxDate}
-                        maxDate={maxDate}
-                        minDate={minDate}
-                        onChange={(date) => handleEndDateChange(date)}
-                        calendarStartDay={1}//sets datepicker week day one
-                    />
+                    <div className="data-picker-navbar-element">
+                        <DatePicker
+                            dateFormat={Constants.DATE_FORMAT}
+                            showIcon
+                            selected={selectedEndDate || maxDate}
+                            maxDate={maxDate}
+                            minDate={minDate}
+                            onChange={(date) => handleEndDateChange(date)}
+                            calendarStartDay={1}//sets datepicker week day one
+                            locale='ru'
+                        />
+                    </div>
+                    </div>
                 </Col>
             </Row>
-        </div>
+        </div >
     );
 }
 
